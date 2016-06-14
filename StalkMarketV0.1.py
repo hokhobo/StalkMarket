30fc84874f5f4c6dfe1654d2f4f8885d3ba4bc59
hokhobo <biz.jrendon@gmail.com>
2016-06-14 10:44:53 -0600

    Update StalkMarketV0.1.py

--- StalkMarketV0.1.py
+++ StalkMarketV0.1.py
@@ -17,8 +17,7 @@ class Card:
   
 #Deck
 class Deck:
- def __init__(self, decktype):
-  self.decktype = decktype
+ def __init__(self):
   self.pile = []
   
  def addCard(self, card):
@@ -27,7 +26,7 @@ class Deck:
  def removeCard(self):
   self.pile.remove(pile[0])
 
- def shuffleDeck(self):
+ def shuffle(self):
   self.pile = self.pile
   
   
@@ -39,8 +38,9 @@ class Portfolio:
  def addStock(self, stock, quant):
   self.holdings.append([stock, quant])
 
- def updateQuant(self, ID, chg):
-  self.holdings[ID][1] += chg
+ def updateQuant(self, stock, chg):
+  self.holdings[stock][1] += chg
+  
   
 #Player 
 class Player:
@@ -55,11 +55,102 @@ class Player:
   
  def updatePortfolio(self):
   self.positions = self.positions
+  
+ def showPortfolio(self):
+  print(Player1.ID)
+  print("Cash: " + str(Player1.cash))
+  for x in self.positions.holdings:
+   print(x[0].ticker + " " + str(x[1]))
+   
+ def drawCard(self, card):
+  self.hand.append(card)
+  
+ def playCard(self, card):
+  self.hand.delete(card)
 
- def drawCard(self):
-  self.hand = self.hand
+ def updateCash(self, chg):
+  self.cash += chg
+
+ def trade(self):
+  self.positions = self.positions
+  
+ def buy(self, stock, quant):
+  self.positions.updateQuant(stock, quant)
+  self.Updatecash(-1*quant*stock.price)
+
+ def sell(self, stock, quant):
+  self.positions.updateQuant(stock, -quant)
+  self.Updatecash(quant*stock.price)
+  
+#Methods
+def initStocks():
+ crt = Stock("CRT", 5, "Sp", "Su")
+ cbg = Stock("CBG", 6, "Sp", "Fa")
+ mln = Stock("MLN", 7, "Su", "Sp")
+ rds = Stock("RDS", 8, "Su", "Fa")
+ crn = Stock("CRN", 9, "Fa", "Sp")
+ apl = Stock("APL", 10, "Fa", "Su")
+ return [crt, cbg, mln, rds, crn, apl]
+
+def initCards():
+ bulls = Deck()
+ for i in range(50):
+  bulls.addCard(Card("bull","NA"))
+  
+ bears = Deck()
+ for i in range(50):
+  bears.addCard(Card("bear","NA"))
+             
+ market = Deck()
+ for i in range(100):
+  market.addCard(Card("market","NA"))
+             
+ bulls.shuffle()
+ bears.shuffle()               
+ market.shuffle()
+ return [bulls, bears, market]
+
+def stockHelper(stocks, symb):
+ for stock in stocks:
+  if stock.ticker == symb:
+   break
+  else:
+   stock = None
+ return stock
+
+def marketPhase(player):
+ inPhase = True             
+ while inPhase :
+  mktcmd = input("What do you wish to buy? ")
+  if mktcmd == "End" :
+    inPhase = False
+    continue                                 
+  else:
+   action = mktcmd[0]
+   stock = mktcmd [2:5]
+   quant = int(mktcmd[6:])
+   if action == "L":
+    player.buy(stockHelper(player.positions.holdings[0:][0], stock), quant)
+   print(str(quant) + " " + stock + " " + action)
+   continue
 
 
+#Main
+
+#Create Stocks
+stocklist = initStocks()
+#Create Cards
+decklist = initCards()
+#Create Players
+Player1 = Player("Boar")
+#Add funds
+Player1.updateCash(100)
+#Create Portfolio
+for stock in stocklist:
+ Player1.positions.addStock(stock, 0)
+
+marketPhase(Player1)
+Player1.showPortfolio()
 
 
 
@@ -68,20 +159,20 @@ class Player:
 
 #Object and Function Testing
 #Stock test#
-carrot = Stock("CRT", 8, "Spring", "Winter")
-print(carrot.ticker)
-print(carrot.price)
-print(carrot.good)
-print(carrot.bad)
-carrot.chgPrice(2)
-print(carrot.price)
-carrot.chgPrice(-4)
-print(carrot.price)
+#carrot = Stock("CRT", 8, "Spring", "Winter")
+#print(carrot.ticker)
+#print(carrot.price)
+#print(carrot.good)
+#print(carrot.bad)
+#carrot.chgPrice(2)
+#print(carrot.price)
+#carrot.chgPrice(-4)
+#print(carrot.price)
 
 #Portfolio test
-porttest = Portfolio()
-print(porttest.holdings)
-porttest.addStock(carrot, 1)
-print(porttest.holdings[0][0].ticker + " " + str(porttest.holdings[0][1]))
-porttest.updateQuant(0, 5)
-print(porttest.holdings[0][0].ticker + " " + str(porttest.holdings[0][1]))
+#porttest = Portfolio()
+#print(porttest.holdings)
+#porttest.addStock(carrot, 1)
+#print(porttest.holdings[0][0].ticker + " " + str(porttest.holdings[0][1]))
+#porttest.updateQuant(0, 5)
+#print(porttest.holdings[0][0].ticker + " " + str(porttest.holdings[0][1]))
