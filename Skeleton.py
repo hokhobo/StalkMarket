3ffd8b68a852209e9acfcadf363fe8ad18f4fbca
hokhobo <biz.jrendon@gmail.com>
2016-06-15 23:30:27 -0600

    Add files via upload
    Working version of the game

--- Skeleton.py
+++ Skeleton.py
@@ -1,3 +1,4 @@
+###SET UP ELEMENTS### AKA SKELETON
 import random
 
 movdice = (-1, -1, 0, 1, 1, 1)
@@ -11,12 +12,28 @@ class Stock:
   self.price = price
   self.good = good
   self.bad = bad
+  self.lastchg = 0
   
  def mktMove(self, turn):
   chg = random.choice(movdice)*random.choice(magdice)
   #Compare stock season to current season
   self.price += chg
+  self.lastchg = chg
 
+crt = Stock("CRT", 5, "sp", "su")
+cbg = Stock("CBG", 6, "sp", "fa")
+mln = Stock("MLN", 7, "su", "fa")
+crn = Stock("CRN", 8, "su", "sp")
+rds = Stock("RDS", 9, "fa", "su")
+apl = Stock("APL", 10, "fa", "sp")
+stocklist = [crt, cbg, mln, crn, rds, apl]
+
+def tickerBoard():
+ print("Stock " + "Price " + "Last Move")
+ for stock in stocklist:
+  print(stock.ticker + " " + str(stock.price) + " "
+        + str(stock.lastchg))
+    
 #Card
 class Card:
  def __init__(self, cardtype, effect):
@@ -26,25 +43,46 @@ class Card:
   self.title = "NA"
   self.hidden = True
 
-#Liability
-class Liability:
- def __init__(self, stock, quant, start):
-  self.stock = stock
-  self.qunat = quant
-  self.start = start
-  self.end = start + 2
- def expire(self):
-  None
+#Makes a new Deck object with *amt *cardtype cards
+#then shuffles it
+def initDeck(amt, cardtype):
+ deck = []
+ for i in range(amt):
+  deck.append(Card(cardtype,"NA"))
+ random.shuffle(deck) 
+ return deck
+
+bears = initDeck(50, "bears")
+bulls = initDeck(50, "bulls")
+market = initDeck(100, "market")
+discard = initDeck(0, None)
+stack = initDeck(0, None)
+
   
 #Turn
 class Turn:
  def __init__(self):
   self.turnnum = 0
   self.season = ""
+ def happen(self):
+  None#All the components of a turn
   
+#Liability
+class Liability:
+ def __init__(self, stock, quant, start, owner):
+  self.stock = stock
+  self.quant = quant
+  self.start = start.turnum
+  self.end = start + 2
+  self.owner = owner
+ def expire(self, turn):
+  if self.end == turn.turnnum:
+   self.owner.buy(self.stock, self.quant)
+   self.owner.contracts.remove(self)
+
 #Player 
 class Player:
- def __init__(self, ID, stocklist):
+ def __init__(self, ID):
   self.ID = ID
   self.cash = 100
   self.hand = []
@@ -52,10 +90,15 @@ class Player:
   self.positions = []
   for stocks in stocklist:
    self.positions.append([stocks, 0])
-  
+  self.networth = 0
+
 #Show name, cash, and positions of player  
  def showPortfolio(self):
+  self.networth = sum([self.cash,
+  sum([a*b for a,b in zip([itema[0].price for itema in self.positions],
+  [itemb[1] for itemb in self.positions])])])  
   print(self.ID)
+  print("Networth: " + str(self.networth))
   print("Cash: " + str(self.cash))
   for x in self.positions:
    print(x[0].ticker + " " + str(x[1]))
@@ -80,15 +123,38 @@ class Player:
   
 #Changes *stock by *quant and cash by *quant x (stock price)
 #Buy *quant > 0, Sell *quant < 0
- def buyorsell(self, stock, quant):
+ def buy(self, stock, quant):
   templist = [item[0] for item in self.positions]
   self.positions[templist.index(stock)][1] += quant
   self.cash += (-1*quant*stock.price)
+  
+ def sell(self, stock, quant):
+   self.buy(stock, -quant)
 
-#
-def short(self, stock, quant):
- buyorsell(stock, -quant)
- self.contracts.append(Liability(stock, quant, currentturn.turnnum))
+#Sells *quant *stock then adds Liability for *stock *quant
+ def short(self, stock, quant, turn):
+  sell(stock, quant)
+  self.contracts.append(Liability(stock, quant, turn, self))
  
- 
+   
+###GAMEPLAY ELEMENTS### AKA MUSCLE
+
+def market(player):
+ inPhase = True             
+ while inPhase :
+  mktcmd = input("Welcome to the market: ")
+  if mktcmd == "End" :
+    inPhase = False
+    continue                                 
+  else:
+   cmds = mktcmd.split(maxsplit=3)
+   eval("player."+cmds[0]+"("+cmds[2]+", "+cmds[1]+")")
+   player.showPortfolio()
+   continue
+
+
+
+
+
+
    
--- StalkMarketV0.1.py
+++ StalkMarketV0.1.py
@@ -1,139 +1,34 @@
-#Defining object classes
+import random
+import Skeleton
 
-#Stock
-class Stock:
- def __init__(self, ticker, price, good, bad):
-  self.ticker = ticker
-  self.price = price
-  self.good = good
-  self.bad = bad
- def chgP(self, chg):
-  self.price += chg
+####Game Set Up####
 
-#Card
-class Card:
- def __init__(self, cardtype, effect):
-  self.cardtype = cardtype
-  self.effect = effect
-  self.hidden = True
-  
-#Player 
-class Player:
- def __init__(self, ID):
-  self.ID = ID
-  self.cash = 0
-  self.positions = []
-  self.hand = []
+#Add players#
+currentturn = Skeleton.Turn()
+playlist = []
+players = int(input("How many players? "))
+for x in range(players):
+ name = input("Enter player's name: ")
+ playlist.append(Skeleton.Player(name))
 
-#Create a list of stocks from *stocklist and set quantities to 0
- def buildPortfolio(self, stocklist):
-  for stocks in stocklist:
-   self.positions.append([stocks, 0])
-  self.cash += 100
+#Each player draws cards
+##for player in playlist:
+## for i in range(3):
+##  deck = input("What deck would you like to draw from? ")
+##  eval("player.drawCard(Skeleton."+deck+")")
+##
+#Show Portfolio and allow them to buy
+for player in playlist:
+## print("This is how you start out!")
+## player.showPortfolio()
+ print("Build your portfolio!")
+ Skeleton.market(player)
 
-#Show name, cash, and positions of player  
- def showPortfolio(self):
-  print(Player1.ID)
-  print("Cash: " + str(Player1.cash))
-  for x in self.positions:
-   print(x[0].ticker + " " + str(x[1]))
-   
-#Draw from *deck and put into hand  
- def drawCard(self, deck):
-  self.hand.append(deck.pop())
+#Move the market
+for stock in Skeleton.stocklist:
+ stock.mktMove(currentturn)
+Skeleton.tickerBoard()
 
-#Player removes* card from hand and adds it to play stack  
- def playCard(self, card):
-  stack.append(card)
-  self.hand.remove(card)
-
-#Current player gives *quant1 *stock1 to player *other for *quant2 *stock2
- def trade(self, other, stock1, quant1, stock2, quant2):
-  templist1 = [item[0] for item in self.positions]
-  templist2 = [item[0] for item in other.positions]
-  self.positions[templist1.index(stock1)][1] -= quant1
-  self.positions[templist2.index(stock2)][1] += quant2
-  other.positions[templist2.index(stock2)][1] -= quant
-  other.positions[templist1.index(stock1)][1] += quant
-  
-#Increases *stock by *quant, while decreasing cash holdings
- def buy(self, stock, quant):
-  templist = [item[0] for item in self.positions]
-  self.positions[templist.index(stock)][1] += quant
-  self.cash += (-1*quant*stock.price)
-  
-#Decrease *stock by *quant, while increasing cash holdings
- def sell(self, stock, quant):
-  templist = [item[0] for item in self.positions]
-  self.positions[templist.index(stock)][1] -= quant
-  self.cash += (quant*stock.price)
-  
-#Methods
-
-#initDeck makes a new Deck object with *amt *cardtype cards then shuffles it
-def initDeck(amt, cardtype):
- deck = []
- for i in range(amt):
-  deck.append(Card(cardtype,"NA"))
- return deck
-
-#shuffle randomizes *deck
-def shuffle(deck):
- None
- 
-###marketPhase lets the player interact with the market
-##def marketPhase(player):
-## inPhase = True             
-## while inPhase :
-##  mktcmd = input("What do you wish to buy? ")
-##  if mktcmd == "End" :
-##    inPhase = False
-##    continue                                 
-##  else:
-##   action = mktcmd[0]
-##   stock = mktcmd [2:5]
-##   quant = int(mktcmd[6:])
-##   if action == "L":
-##    player.buy(stockHelper(player.positions.holdings[0:][0], stock), quant)
-##   print(str(quant) + " " + stock + " " + action)
-##   continue
-
-
-#Main (TEST)
-
-#Create Stocks
-crt = Stock("CRT", 5, "Sp", "Su")
-cbg = Stock("CBG", 6, "Sp", "Fa")
-mln = Stock("MLN", 7, "Su", "Sp")
-rds = Stock("RDS", 8, "Su", "Fa")
-crn = Stock("CRN", 9, "Fa", "Sp")
-apl = Stock("APL", 10, "Fa", "Su")
-stocklist = [crt, cbg, mln, rds, crn, apl]
-#Create Decks
-bears = initDeck(50, "bears")
-bulls = initDeck(50, "bulls")
-market = initDeck(100, "market")
-discard = initDeck(0, None)
-stack = initDeck(0, None)
-
-#Create Players
-Player1 = Player("Boar")
-Player1.buildPortfolio(stocklist)
-#Create Portfolio
-
-
-
-
-#Testing
-Player1.buy(crt, 5)
-Player1.sell(crt, 5)
-Player1.showPortfolio()
-Player1.drawCard(bears)
-print(Player1.hand[0].cardtype)
-Player1.playCard(Player1.hand[0])
-print(Player1.hand)
-print(stack[0].cardtype)
-
-
-
-
+#Show results
+for player in playlist:
+ player.showPortfolio()
